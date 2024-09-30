9d6604b990dae4ae662d5a16cd9773671678dede
kodok <77190656+uzidreamcode@users.noreply.github.com>
2024-10-01 00:57:41 +0700

    fix all praktikum

---
+++ lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FabWidget extends StatelessWidget {
+  const FabWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
